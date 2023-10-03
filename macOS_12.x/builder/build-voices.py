f8483eff8a9bc446bcad635bc45ddab37fb1a801
Alan Smith <commit@alanwsmith.com>
2023-10-03 17:05:54 -0400

    Sorted

--- macOS_12.x/builder/build-voices.py
+++ macOS_12.x/builder/build-voices.py
@@ -8,39 +8,48 @@ with open("../voices.txt") as _voices:
 
 output = ""
 
-for voice in voices:
-    matches = re.search(r'(.*?)\s+\S+\s+#\s+(.*)', voice)
+voice_list = []
+
+for line in voices:
+    matches = re.search(r'(.*?)\s+(\S+)\s+#\s+(.*)', line)
     if matches:
         name = matches.group(1)
         name_lc = name.lower()
-        text = matches.group(2)
+        lang = matches.group(2)
+        text = matches.group(3)
+        voice_list.append((lang, name, name_lc, text))
 
-        print(f"aiff: {name_lc}")
-        aiff_command = [
-            "say",
-            "--voice",
-            name,
-            text,
-            "-o",
-            f"../aiffs/{name_lc}.aiff"
-        ]
-        subprocess.run(aiff_command)
+voice_list.sort(key=lambda x: x[0])
 
-        print(f"mp3:  {name_lc}")
-        mp3_command = [
-            "ffmpeg",
-            "-i",
-            f"../aiffs/{name_lc}.aiff",
-            "-y",
-            f"../mp3s/{name_lc}.mp3",
-        ]
-        subprocess.run(mp3_command)
+for voice in voice_list:
+    print(voice)
 
-        output += f"""<div class="voice_wrapper">
-  <div class="voice_name">{name_lc}</div>
+    print(f"aiff: {voice[2]}")
+    aiff_command = [
+        "say",
+        "--voice",
+        voice[1],
+        voice[3],
+        "-o",
+        f"../aiffs/{voice[2]}.aiff"
+    ]
+    subprocess.run(aiff_command)
+
+    print(f"mp3:  {voice[2]}")
+    mp3_command = [
+        "ffmpeg",
+        "-i",
+        f"../aiffs/{voice[2]}.aiff",
+        "-y",
+        f"../mp3s/{voice[2]}.mp3",
+    ]
+    subprocess.run(mp3_command)
+
+    output += f"""<div class="voice_wrapper">
+  <div class="voice_name">{voice[0]} - {voice[2]}</div>
   <div class="voice_sample">
     <audio controls preload="none">
-      <source src="https://github.com/alanwsmith/text-to-speech-voices/blob/main/macOS_12.x/mp3s/{name_lc}.mp3?raw=true" type="audio/mpeg" />
+    <source src="https://github.com/alanwsmith/text-to-speech-voices/blob/main/macOS_12.x/mp3s/{voice[2]}.mp3?raw=true" type="audio/mpeg" />
     </audio>
   </div>
 </div>
